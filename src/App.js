7c93274232f4b55f137ada94a46a3724db5b2a7f
EnableDarkTheme <berkovicammar1@gmail.com>
2024-10-01 21:54:38 +0200

    misa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from "./dena.png";
+import jaha from "./jaha.jpg";
+import mia from "./mia.jpg";
+import ahmed from "./ahmed.jpg";
 import "./App.css";
 
 function App() {
@@ -18,13 +21,22 @@ function App() {
         <p>Svaka cast Dena, dobro si je otkrio! (scroll)</p>
         <p>Ovo je tvoj sljedeci clue:</p>
         <div className="non-spinning">
-          <img src="https://www.domprojekt.hr/upload/tbl_tip_kuca/02_montazne_potkrovlje_232346.jpg.axd?width=800&height=470&crop=auto&quality=80" className="no-logo" alt="logo" />
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdxf_Duggbx8fKVv-EXZTbeQmX63Vbnavkzg&s" className="no-logo" alt="logo" />
-          <img src={logo} className="no-logo" alt="logo" />
+          <img
+            src="https://www.domprojekt.hr/upload/tbl_tip_kuca/02_montazne_potkrovlje_232346.jpg.axd?width=800&height=470&crop=auto&quality=80"
+            className="no-logo"
+            alt="logo"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdxf_Duggbx8fKVv-EXZTbeQmX63Vbnavkzg&s"
+            className="no-logo"
+            alt="logo"
+          />
+          <img src={jaha} className="no-logo" alt="logo" />
+          <span>a</span>
         </div>
         <div className="non-spinning">
-          <img src={logo} className="no-logo" alt="logo" />
-          <img src={logo} className="no-logo" alt="logo" />
+          <img src={ahmed} className="mia" alt="logo" />
+          <img src={mia} className="mia" alt="logo" />
         </div>
       </header>
     </div>
